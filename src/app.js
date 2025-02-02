58f832db0226fe297899c9a67becf78329953955
Erik-Galstyan <erik.galstyan26@gmail.com>
2025-02-02 17:30:10 +0400

    add static folder

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.send()
-})
+app.use(express.static(join(__dirname, '../public')));
+
+app.get("/", (req, res, next) => {
+  res.sendFile(join(__dirname, "view", "index.html"));
+});
 
 app.listen(3000);
